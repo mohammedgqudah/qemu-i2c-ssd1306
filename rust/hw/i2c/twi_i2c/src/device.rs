12e3b4d4e1d28f96f836ea24e3cd85af71ab2502
Mohammed AlQudah <mohghq@gmail.com>
2025-03-12 01:24:59 +0300

    Use ResettablePhasesImpl and BqlRefCell for registers

--- rust/hw/i2c/twi_i2c/src/device.rs
+++ rust/hw/i2c/twi_i2c/src/device.rs
@@ -11,7 +11,8 @@ use qemu_api::{
         SysBusDevice,
     },
     c_str,
-    qdev::{DeviceImpl, Property},
+    cell::BqlRefCell,
+    qdev::{DeviceImpl, Property, ResetType, ResettablePhasesImpl},
     qom::{ClassInitImpl, ObjectImpl, ObjectType, ParentField},
     sysbus::SysBusDeviceClass,
     vmstate::VMStateDescription,
@@ -20,39 +21,32 @@ use qemu_api_macros::Object;
 
 use crate::{memory_ops::TWI_I2C_OPS, registers};
 
-#[derive(Debug, Object, qemu_api_macros::offsets)]
+#[derive(Object, qemu_api_macros::offsets)]
 #[repr(C)]
-pub struct TWI_I2CState {
+pub struct TWIState {
     pub parent_obj: ParentField<SysBusDevice>,
     pub iomem: MemoryRegion,
     pub someprop: char,
     pub bus: *mut I2CBus,
     pub irq: qemu_irq,
 
-    pub twbr: registers::TWBR,
-    pub twsr: registers::TWSR,
-    pub twar: registers::TWAR,
-    pub twdr: registers::TWDR,
-    pub twcr: registers::TWCR,
-
+    pub registers: BqlRefCell<registers::TWIRegisters>,
     pub enabled: bool,
-    // TODO: is there a way to track this using only the registers?
-    pub in_transaction: bool,
 }
 
-unsafe impl ObjectType for TWI_I2CState {
+unsafe impl ObjectType for TWIState {
     type Class = TWI_I2CClass;
     const TYPE_NAME: &'static CStr = crate::TYPE_TWI_I2C;
 }
 
-impl ObjectImpl for TWI_I2CState {
+impl ObjectImpl for TWIState {
     type ParentType = SysBusDevice;
 
     const INSTANCE_INIT: Option<unsafe fn(&mut Self)> = Some(Self::init);
     const INSTANCE_POST_INIT: Option<fn(&Self)> = None;
 }
 
-impl DeviceImpl for TWI_I2CState {
+impl DeviceImpl for TWIState {
     fn properties() -> &'static [Property] {
         &crate::device_class::TWI_I2C_PROPERTIES
     }
@@ -60,10 +54,13 @@ impl DeviceImpl for TWI_I2CState {
         Some(&crate::device_class::VMSTATE_TWI_I2C)
     }
     const REALIZE: Option<fn(&Self)> = Some(Self::realize);
-    const RESET: Option<fn(&Self)> = Some(Self::reset);
 }
 
-impl TWI_I2CState {
+impl ResettablePhasesImpl for TWIState {
+    const HOLD: Option<fn(&Self, ResetType)> = Some(Self::reset_hold);
+}
+
+impl TWIState {
     /// Initializes a pre-allocated, unitialized instance of `TWI_I2CState`.
     ///
     /// # Safety
@@ -103,23 +100,13 @@ impl TWI_I2CState {
         }
     }
 
-    /// Reset the TWI controller registers.
-    ///
-    /// Based on "Register Description" in the ATmega640/1280/1281/2560/2561
-    /// Datasheet.
-    pub fn reset(&self) {
+    /// Reset the TWI controller.
+    pub fn reset_hold(&self, _type: ResetType) {
         println!("reset");
-        unsafe {
-            // TODO: ONLY UNTIL I START USING THE BGL OR CELL.
-            let s = addr_of!(self) as *mut Self;
-            let s = &mut *s as &mut Self;
-            s.twbr = 0.into();
-            s.twsr = 0b11111000.into();
-            s.twar = 0b11111110.into();
-            s.twdr = 0xFF.into();
-            s.twcr = 0.into();
-            s.in_transaction = false;
-        }
+        self.registers.borrow_mut().reset();
+        //unsafe {
+        //    s.in_transaction = false;
+        //}
     }
 
     pub fn read(&mut self, offset: hwaddr, _size: c_uint) -> u64 {
@@ -129,7 +116,7 @@ impl TWI_I2CState {
                 //println!("reading: reg 0");
             }
             1 => {
-                let x: u8 = self.twsr.into();
+                let x: u8 = self.registers.borrow().twsr.into();
                 return u64::from(x);
             }
             2 => {
@@ -140,7 +127,7 @@ impl TWI_I2CState {
             }
             4 => {
                 //println!("reading: reg 4");
-                let x: u8 = self.twcr.into();
+                let x: u8 = self.registers.borrow().twcr.into();
                 return u64::from(x);
             }
             5 => {
@@ -155,6 +142,7 @@ impl TWI_I2CState {
 
     pub fn write(&mut self, address: hwaddr, data: u8) {
         //println!("write address: {}: data: {}", address, data);
+        let mut registers = self.registers.borrow_mut();
         match address {
             0 => {
                 // set the bit rate
@@ -167,18 +155,22 @@ impl TWI_I2CState {
             }
             2 => {
                 // set address
-                self.twar = registers::TWAR::from(data);
+                registers.twar = registers::TWAR::from(data);
                 // TODO: handle the LSB
-                println!("slave address set: {}", u8::from(self.twar) >> 1);
+                println!("slave address set: {}", u8::from(registers.twar) >> 1);
             }
             3 => {
                 // set data
-                self.twdr = registers::TWDR::from(data);
+                registers.twdr = registers::TWDR::from(data);
                 //println!(
                 //    "char: {} ; hex: {:X}",
                 //    u8::from(self.twdr) as char,
                 //    u8::from(self.twdr)
                 //);
+
+                // Release the BqlRefCell before calling write_data
+                drop(registers);
+
                 self.write_data();
             }
             4 => {
@@ -186,31 +178,32 @@ impl TWI_I2CState {
                 //println!("{:#?}", r);
                 // TODO: if this bit is reset, terminate all on going trasmissions
                 self.enabled = r.twen();
-                self.twcr.set_twint(r.twint());
+                registers.twcr.set_twint(r.twint());
 
                 if r.twsta() {
-                    self.twcr.set_twsta(true);
+                    registers.twcr.set_twsta(true);
                 }
 
                 if r.twsto() {
                     //println!("Going to stop");
+                    drop(registers);
                     self.stop();
                 } else if r.twint() && r.twen() {
                     // TODO: The global interrupt must be enabled SREG (I)
 
                     // This is a new transaction
-                    if !self.in_transaction {
+                    if !registers.in_transaction {
                         // TODO: don't touch the first two bits when setting the status
-                        self.in_transaction = true;
-                        self.twsr = registers::TW_START.into();
-                        self.twcr.set_twen(true);
+                        registers.in_transaction = true;
+                        registers.twsr = registers::TW_START.into();
+                        registers.twcr.set_twen(true);
                     }
 
                     unsafe {
                         // TODO: use the new InterruptSource API
                         qemu_set_irq(self.irq, 1);
                     };
-                    self.twcr.set_twint(true); // Fake TWI
+                    registers.twcr.set_twint(true); // Fake TWI
                 }
             }
             _ => {
@@ -224,36 +217,43 @@ impl TWI_I2CState {
     const fn _start(&self) {}
 
     /// Handle a STOP condition.
-    fn stop(&mut self) {
+    fn stop(&self) {
+        let mut registers = self.registers.borrow_mut();
         unsafe {
             i2c_end_transfer(self.bus);
             //println!("ended transfer");
         };
-        self.in_transaction = false;
-        self.twcr.set_twsto(false); // report that STOP has executed on the bus.
-        self.twcr.set_twint(false); // TODO: according to twi_stop
-                                    // in the arduino library, confirm
-                                    // in
-                                    // the data sheet
+        registers.in_transaction = false;
+        registers.twcr.set_twsto(false); // report that STOP has executed on the bus.
+        registers.twcr.set_twint(false); // TODO: according to twi_stop
+                                         // in the arduino library, confirm
+                                         // in
+                                         // the data sheet
     }
-    fn write_data(&mut self) {
+    fn write_data(&self) {
+        let mut registers = self.registers.borrow_mut();
         // report that you cannot write when twint is low
-        if !self.twcr.twint() {
-            self.twcr.set_twwc(true);
+        if !registers.twcr.twint() {
+            registers.twcr.set_twwc(true);
             return;
         }
 
-        if self.twcr.twsta() {
+        if registers.twcr.twsta() {
             // If the START bit was set, then this is the first data in the transaction
             // and it contains the sla+w (slave address + w bit)
             //
             // TODO: THIS IS NOT ALWAYS TRUE, in repeated start, the address wont be sent
             // again. (not sure)
-            self.twcr.set_twsta(false);
-            let start_read = self.twdr.twd0(); // write bit
-                                               // TODO: do i use start_transfer when attempting to read?
+            registers.twcr.set_twsta(false);
+            let start_read = registers.twdr.twd0(); // write bit
+                                                    // TODO: do i use start_transfer when attempting to read?
 
-            match i2c_start_transfer(self.bus, u8::from(self.twdr) >> 1, false) {
+            let address = u8::from(registers.twdr) >> 1;
+
+            // Release the BqlRefCell before calling set_status
+            drop(registers);
+
+            match i2c_start_transfer(self.bus, address, false) {
                 Ok(()) => {
                     self.set_status(if start_read {
                         registers::TW_MR_SLA_ACK
@@ -279,7 +279,11 @@ impl TWI_I2CState {
             }
         } else {
             // TODO: refactor to a Result<(), ()>
-            let resp = unsafe { i2c_send(self.bus, self.twdr.into()) };
+            let resp = unsafe { i2c_send(self.bus, registers.twdr.into()) };
+
+            // Release the BqlRefCell before calling set_status
+            drop(registers);
+
             if resp == 0 {
                 self.set_status(registers::TW_MT_DATA_ACK);
             } else {
@@ -289,9 +293,9 @@ impl TWI_I2CState {
     }
 
     /// Set the status bits in TWSR.
-    fn set_status(&mut self, status: u8) {
+    fn set_status(&self, status: u8) {
         // TODO: only modify the last 5 bits.
-        self.twsr = status.into();
+        self.registers.borrow_mut().twsr = status.into();
     }
 }
 
@@ -312,7 +316,7 @@ pub struct TWI_I2CClass {
     parent_class: <SysBusDevice as ObjectType>::Class,
 }
 
-impl ClassInitImpl<TWI_I2CClass> for TWI_I2CState {
+impl ClassInitImpl<TWI_I2CClass> for TWIState {
     fn class_init(klass: &mut TWI_I2CClass) {
         <Self as ClassInitImpl<SysBusDeviceClass>>::class_init(&mut klass.parent_class);
     }
@@ -327,7 +331,7 @@ pub unsafe extern "C" fn twi_i2c_init(obj: *mut Object) {
     unsafe {
         // TODO: replace debug_assert with new instead of using new_unchecked
         debug_assert!(!obj.is_null());
-        let mut state = NonNull::new_unchecked(obj.cast::<TWI_I2CState>());
+        let mut state = NonNull::new_unchecked(obj.cast::<TWIState>());
         state.as_mut().init();
     }
 }
--- rust/hw/i2c/twi_i2c/src/device_class.rs
+++ rust/hw/i2c/twi_i2c/src/device_class.rs
@@ -1,15 +1,13 @@
-use core::ptr::NonNull;
-
 use qemu_api::{bindings::*, c_str, vmstate_fields, vmstate_unused, zeroable::Zeroable};
 
-use crate::device::TWI_I2CState;
+use crate::device::TWIState;
 
 // TODO: Find a way to not define properties
 qemu_api::declare_properties! {
     TWI_I2C_PROPERTIES,
     qemu_api::define_property!(
         c_str!("someprop"),
-        TWI_I2CState,
+        TWIState,
         someprop,
         unsafe { &qdev_prop_chr },
         CharBackend
@@ -28,28 +26,28 @@ pub static VMSTATE_TWI_I2C: VMStateDescription = VMStateDescription {
     ..Zeroable::ZERO
 };
 
-/// # Safety
-///
-/// We expect the FFI user of this function to pass a valid pointer, that has
-/// the same size as [`TWI_I2CState`]. We also expect the device is
-/// readable/writeable from one thread at any time.
-pub unsafe extern "C" fn twi_i2c_realize(dev: *mut DeviceState, _errp: *mut *mut Error) {
-    unsafe {
-        assert!(!dev.is_null());
-        let mut state = NonNull::new_unchecked(dev.cast::<TWI_I2CState>());
-        state.as_mut().realize();
-    }
-}
+// # Safety
+//
+// We expect the FFI user of this function to pass a valid pointer, that has
+// the same size as [`TWI_I2CState`]. We also expect the device is
+// readable/writeable from one thread at any time.
+//pub unsafe extern "C" fn twi_i2c_realize(dev: *mut DeviceState, _errp: *mut *mut Error) {
+//    unsafe {
+//        assert!(!dev.is_null());
+//        let mut state = NonNull::new_unchecked(dev.cast::<TWIState>());
+//        state.as_mut().realize();
+//    }
+//}
 
-/// # Safety
-///
-/// We expect the FFI user of this function to pass a valid pointer, that has
-/// the same size as [`TWI_I2CState`]. We also expect the device is
-/// readable/writeable from one thread at any time.
-pub unsafe extern "C" fn twi_i2c_reset(dev: *mut DeviceState) {
-    unsafe {
-        assert!(!dev.is_null());
-        let mut state = NonNull::new_unchecked(dev.cast::<TWI_I2CState>());
-        state.as_mut().reset();
-    }
-}
+// # Safety
+//
+// We expect the FFI user of this function to pass a valid pointer, that has
+// the same size as [`TWI_I2CState`]. We also expect the device is
+// readable/writeable from one thread at any time.
+//pub unsafe extern "C" fn twi_i2c_reset(dev: *mut DeviceState) {
+//    unsafe {
+//        assert!(!dev.is_null());
+//        let mut state = NonNull::new_unchecked(dev.cast::<TWIState>());
+//        state.as_mut().reset_hold();
+//    }
+//}
